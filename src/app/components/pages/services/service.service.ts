194dccc76de7067bfb9bac5308f57ed8d169d1db
hinh27072000 <hinh.dx2k@gmail.com>
2023-03-22 17:49:00 +0700

    done chatgpt

--- src/app/components/pages/services/service.service.ts
+++ src/app/components/pages/services/service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -28,4 +29,15 @@ export class ServiceService {
   getAppoint(id:any){
     return this.http.get(this.url + "/appointment/getByCategory/"+ id,{ })
   }
+  
+  chat(mess: any) {
+    let SECRET_KEY = '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'
+    let messages: any = [{ role: "user", content: mess }]
+    let param: any = {
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        temperature: 0.5
+    }
+    return this.http.post<any>('https://api.openai.com/v1/chat/completions', param, {headers:new HttpHeaders().set('Authorization', `Bearer ${SECRET_KEY}`)}) ;
+}
 }
